6cd4c5fa92401b52814852bf7049096b29e67d00
Ansh Kumar Pachauri <anshpachauri84@gmail.com>
2025-11-03 14:36:54 +0530

    landing-page-added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,19 @@ import { SignedIn, SignedOut, useAuth , SignInButton, SignOutButton ,UserButton,
 import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import ProblemsPage from './pages/ProblemsPage.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 
 function App() {
-  const {isSignedIn} = useUser();
+  const {isSignedIn,isLoaded} = useUser();
+
+  if(!isLoaded){
+    return null;
+  }
   return (
     <>
     <Routes>
-      <Route path = "/" element = {<HomePage/>} />
+      <Route path = "/" element = {!isSignedIn ? <HomePage/> : <Navigate to={'/dashboard'}/>} />
+      <Route path = "/dashboard" element = {isSignedIn ? <Dashboard/> : <Navigate to={'/'}/>} />
       <Route path = "/about" element = {<AboutPage/>} />
       <Route path = "/problems" element = {isSignedIn ? <ProblemsPage/> : <Navigate to={'/'}/>} />
 
