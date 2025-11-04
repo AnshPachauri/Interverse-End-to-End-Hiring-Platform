ccd94ada39deee367638a2454f83de535094cbfd
Ansh Kumar Pachauri <anshpachauri84@gmail.com>
2025-11-05 01:53:28 +0530

    problem-page-added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import ProblemsPage from './pages/ProblemsPage.jsx'
 import Dashboard from './pages/Dashboard.jsx'
+import ProblemPage from './pages/ProblemPage.jsx'
 
 function App() {
   const {isSignedIn,isLoaded} = useUser();
@@ -22,6 +23,7 @@ function App() {
       <Route path = "/dashboard" element = {isSignedIn ? <Dashboard/> : <Navigate to={'/'}/>} />
       <Route path = "/about" element = {<AboutPage/>} />
       <Route path = "/problems" element = {isSignedIn ? <ProblemsPage/> : <Navigate to={'/'}/>} />
+      <Route path = "/problem/:id" element = {isSignedIn ? <ProblemPage/> : <Navigate to={'/'}/>} />
 
     </Routes>
     <Toaster toastOptions={{duration : 3000}}/>
